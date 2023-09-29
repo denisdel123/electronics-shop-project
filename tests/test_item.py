722fb2b000f2cf3bb8c9aca35ab95c985b95a3ff
denisbelenko <denisdel123@>
2023-09-29 10:22:19 +0800

    покрыл тестами

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def class_test():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(class_test):
+    assert class_test.calculate_total_price() == 200000
+
+
+def test_apply_discount(class_test):
+    assert class_test.all == [['Смартфон', 10000, 20]]
