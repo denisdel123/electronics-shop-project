6296aaef10226369b58411fcad7a74664a3cb92b
denisbelenko <denisdel123@>
2023-10-06 19:26:06 +0800

    выполнил задание 5 написал тесты подписал методы  и классы

--- src/phone.py
+++ src/phone.py
@@ -1,26 +1,40 @@
 from src.item import Item
 
+"""создали дочерний класс телефон который наследуется от Item """
+
 
 class Phone(Item):
+    """Инициализируем поле и принимаем поля от родительского класса"""
+
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int, ):
         super().__init__(name, price, quantity)
         self._number_of_sim = number_of_sim
         self.__name = name
 
+        """Отправляем информацию пользователю"""
+
     def __str__(self):
         return f"{self.__name}"
 
+    """Отправляем информацию разработчику"""
+
     def __repr__(self):
         return f"Phone('{self.__name}', {self.price}, {self.quantity}, {self._number_of_sim})"
+
+    """установили ограничение на сложение классов"""
 
     def __add__(self, other):
         if not isinstance(other, Item):
             raise ValueError('Складывать можно только объекты Item и дочерние от них.')
         return self.quantity + other.quantity
 
+    """считываем кол-во сим карт"""
+
     @property
     def number_of_sim(self):
         return self._number_of_sim
+
+    """задали ограничение на кол-во сим карт"""
 
     @number_of_sim.setter
     def number_of_sim(self, value):
@@ -28,4 +42,3 @@ class Phone(Item):
             self._number_of_sim = value
         else:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
-
--- tests/test_item.py
+++ tests/test_item.py
@@ -4,10 +4,11 @@ from pathlib import Path
 import pytest
 from src.item import Item
 from src.phone import Phone
+from src.keyboard import Keyboard
 
 
 @pytest.fixture()
-def class_test():
+def class_item():
     return Item("Смартфон", 10000, 20)
 
 
@@ -16,39 +17,44 @@ def class_phone():
     return Phone("iPhone 14", 120_000, 5, 2)
 
 
-def test_item_calculate_total_price(class_test):
-    assert class_test.calculate_total_price() == 200000
+@pytest.fixture()
+def class_keyboard():
+    return Keyboard('Dark Project KD87A', 9600, 5)
 
 
-def test_item_apply_discount(class_test):
-    assert class_test.all == [['Смартфон', 10000, 20]]
+def test_item_calculate_total_price(class_item):
+    assert class_item.calculate_total_price() == 200000
 
 
-def test_item_instantiate_from_csv(class_test):
-    assert len(class_test.all) == 1
+def test_item_apply_discount(class_item):
+    assert class_item.all == [['Смартфон', 10000, 20]]
 
 
-def test_item_string_to_number(class_test):
-    assert class_test.string_to_number(10.0) <= 10
-    assert class_test.string_to_number('5.5') == 5
+def test_item_instantiate_from_csv(class_item):
+    assert len(class_item.all) == 1
 
 
-def test_item_repr(class_test):
-    assert repr(class_test) == 'Item(Смартфон, 10000, 20)'
+def test_item_string_to_number(class_item):
+    assert class_item.string_to_number(10.0) <= 10
+    assert class_item.string_to_number('5.5') == 5
 
 
-def test_item_str(class_test):
-    assert str(class_test) == 'Смартфон'
+def test_item_repr(class_item):
+    assert repr(class_item) == 'Item(Смартфон, 10000, 20)'
 
 
-def test_item_add(class_test, class_phone):
-    assert class_test + class_phone == 25
+def test_item_str(class_item):
+    assert str(class_item) == 'Смартфон'
+
+
+def test_item_add(class_item, class_phone):
+    assert class_item + class_phone == 25
     with pytest.raises(AttributeError):
-        class_test + 111
+        class_item + 111
 
 
-def test_phone_add(class_phone, class_test):
-    assert class_phone + class_test == 25
+def test_phone_add(class_phone, class_item):
+    assert class_phone + class_item == 25
     with pytest.raises(ValueError):
         class_phone + 111
 
@@ -66,3 +72,13 @@ def test_phon_number_of_sim(class_phone):
 
     with pytest.raises(ValueError):
         class_phone.number_of_sim = 0
+
+
+def test_keyboard_change_lang(class_keyboard):
+    assert str(class_keyboard) == 'Dark Project KD87A'
+    assert str(class_keyboard.language) == "EN"
+
+    class_keyboard.change_lang()
+    assert class_keyboard.language == 'RU'
+    with pytest.raises(AttributeError):
+        class_keyboard.language = "KZ"
